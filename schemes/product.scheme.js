e7c2aa143cb4c92d23f780a904e104a18226bff1
Carlos Rodriguez <carlosarc10@gmail.com>
2023-10-13 20:38:23 -0300

    añadido schemes a las demas clases

--- schemes/product.scheme.js
+++ schemes/product.scheme.js
@@ -5,20 +5,20 @@ const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri()
 
-const createProductSchema = Joi.object({
+const createProductScheme = Joi.object({
   name: name.required(),
   price: price.required(),
   image: image.required()
 });
 
-const updateProductSchema = Joi.object({
+const updateProductScheme = Joi.object({
   name: name,
   price: price,
   image: image
 });
 
-const getProductSchema = Joi.object({
+const getProductScheme = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+module.exports = { createProductScheme, updateProductScheme, getProductScheme }

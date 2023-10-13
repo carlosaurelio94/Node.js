7eeb701dc156a2cd2ad3eedacb2d625a21cb2203
Carlos Rodriguez <carlosarc10@gmail.com>
2023-10-13 20:32:53 -0300

    Añadir JOI, validaciones

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const ProductsService = require('./../services/products.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemes/product.scheme');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -14,7 +16,7 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validatorHandler(getProductSchema, 'params'), async (req, res, next) => {
   try {
     const id = req.params.id;
     const product = await service.findOne(id);
@@ -24,22 +26,21 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', validatorHandler(createProductSchema, 'body'),async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id', validatorHandler(getProductSchema, 'params'), validatorHandler(updateProductSchema, 'body'),async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body)
     res.json(product)
   } catch(error) {
-    next(error); 
+    next(error);
   }
-
 });
 
 router.delete('/:id', async (req, res) => {
